456e519be814a293f1ab0cf78360d4caa0ee67d8
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-03-19 21:35:18 +0100

    page single id

--- src/components/AfficheSingle.vue
+++ src/components/AfficheSingle.vue
@@ -3,19 +3,18 @@ import { useRoute } from 'vue-router/auto';
 import { defineProps } from 'vue';
 import type { SchemaFilms } from '@/types';
 import { supabase } from '@/supabase';
-import AfficheSingle from '@/components/AfficheSingle.vue';
 const route = useRoute('/maisons/[id]');
-
+defineProps<SchemaFilms>()
 let {data : film, error} = await supabase
     .from('Films')
     .select('*')
     .eq('id', route.params.id)
     .single();
-
 </script>
 
 <template>
     <div>
-        <AfficheSingle v-bind="film" />
+        <h2>{{ titre_film }}</h2>
+        <p>{{ description_film }}</p>
     </div>
 </template>
