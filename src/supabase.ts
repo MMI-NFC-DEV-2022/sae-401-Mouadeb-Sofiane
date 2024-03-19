bffb675c66fd9827955c65102f861054617e4a9d
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-03-19 09:18:20 +0100

    probleme await

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient, type User } from '@supabase/supabase-js'
 import { ref } from 'vue'
 import { type Database } from './supabase-types'
 
@@ -11,15 +11,15 @@ export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
 globalThis.supabase = supabase
 
 // Création d'une référence réactive pour l'utilisateur
-export const user = ref(null);
+export const user = ref<User|null>(null);
 
 // Fonction asynchrone pour récupérer l'utilisateur et s'abonner aux changements d'état d'authentification
 async function fetchUser() {
-  const { data } = await supabase.auth.getUser();
-  user.value = data.user;
-  supabase.auth.onAuthStateChange((event, session) => {
-    user.value = session?.user ?? null;
-  });
+    const { data } = await supabase.auth.getUser();
+    user.value = data.user;
+    supabase.auth.onAuthStateChange((event, session) => {
+        user.value = session?.user ?? null;
+    });
 }
 
 // Invoquer fetchUser directement pour initialiser l'état de l'utilisateur
