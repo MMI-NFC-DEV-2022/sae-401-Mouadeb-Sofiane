e6fb48d779b4bb8a5f44c4bdde3abade62471b7f
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-03-19 08:41:41 +0100

    ,jnh

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,8 @@ import { RouterLink, RouterView } from 'vue-router'
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/simple">Simple</RouterLink>
-      <RouterLink to="/maisons">Maisons</RouterLink>
-      <RouterLink to="/maisons/edit">Créer des maisons</RouterLink>
+      <RouterLink to="/maisons">Films</RouterLink>
+      <RouterLink to="/maisons/edit">Créer des films</RouterLink>
       <RouterLink to="/login-logout">Connexion</RouterLink>
     </nav>
   </header>
