e2b1d618db17d9ee90604ea5f8938dafc5f40485
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-03-25 12:20:26 +0100

    Refactor login-logout button layout

--- src/pages/login-logout.vue
+++ src/pages/login-logout.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import { supabase, user } from '../supabase';
 </script>
-
 <template>
-<div class="flex items-center">
-  <button v-if="user" @pointerdown="supabase.auth.signOut()" class="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-md transition duration-300 ease-in-out">
-    Se déconnecter ({{ user.email }})
-  </button>
-  <button v-else @pointerdown="supabase.auth.signInWithOAuth({ provider: 'github' })" class="bg-gray-800 hover:bg-gray-900 text-white font-semibold py-2 px-4 rounded-md transition duration-300 ease-in-out">
-    Se connecter avec Github
-  </button>
+<div class="pt-72">
+    <button v-if="user" @pointerdown="supabase.auth.signOut()">
+        Se déconnecter 
+    </button>
+    <button v-else @pointerdown="supabase.auth.signInWithOAuth({provider: 'github'})">
+        Se connecter avec Github
+    </button>
 </div>
 </template>
